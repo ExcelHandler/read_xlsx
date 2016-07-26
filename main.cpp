04c0377b3dbb1d5bcce7ae7684f514553893d5df
lucinda-aemilia <lucinda.aemilia@gmail.com>
2016-07-26 10:03:51 +0800

    Successful read attempt

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <QAxWidget>
 #include <QAxObject>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -11,18 +12,17 @@ int main(int argc, char *argv[])
 
     QAxObject *obj = new QAxObject("Excel.Application");
     obj->setProperty("Visible", true);
-    obj->setProperty("Caption", "Hello world");
+    // obj->setProperty("Caption", "Hello world");
     QAxObject *workBooks = obj->querySubObject("Workbooks");
-    //打开已存的excel文件
-    QAxObject *workBook =
-         workBooks->querySubObject("Open(QString)",
+    QAxObject *workBook = workBooks->querySubObject("Open(QString)",
          "C:\\Users\\lenovo\\Documents\\m_Internship\\Personal_Plan_Shi_JW_201607.xlsx");
 
-    workBooks->dynamicCall("Add");
     //QAxObject *workBook = workBooks->querySubObject("Item(const int)", 1);
     QAxObject *sheets = workBook->querySubObject("Sheets");
     QAxObject *sheet = sheets->querySubObject("Item(int)", 1);
     QAxObject *range = sheet->querySubObject("Range(const QVariant&)", QVariant(QString("A1:A1")));
+    qDebug() << range->property("Value");
+
     range->dynamicCall("Clear()");
     range->dynamicCall("SetValue(const QVariant&)", QVariant(5));
     obj->dynamicCall("SetScreenUpdating(bool)", true);
